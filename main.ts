7e44fe31c1087a86740d9a6f4aca3457c0b347a4
davidp443 <82887237+davidp443@users.noreply.github.com>
2021-08-07 10:18:05 +0100

    First version

--- main.ts
+++ main.ts
@@ -1,11 +1,29 @@
+let offset = 0
+let rad = 0
+let z = 0
+let x = 0
+let neswn = images.createBigImage(`
+# . . . # . # # # . . . # # . . # . # .# . . . #
+# # . . # . # . . . . # . . . . # . # .# # . . #
+# . # . # . # # # . . . # . . . # # # .# . # . #
+# . . # # . # . . . . . . # . . # # # .# . . # #
+# . . . # . # # # . . # # . . . # . # .# . . . #
+`)
+let nwsen = images.createBigImage(`
+# . . . # . # . # . . . # # . . # # # .# . . . #
+# # . . # . # . # . . # . . . . # . . .# # . . #
+# . # . # . # # # . . . # . . . # # # .# . # . #
+# . . # # . # # # . . . . # . . # . . .# . . # #
+# . . . # . # . # . . # # . . . # # # .# . . . #
+`)
 basic.forever(function () {
-	let x = input.magneticForce(Dimension.X)
-    let z = input.magneticForce(Dimension.Z)
-    let rad = Math.atan2(x,-z)
-    //let deg = rad * 360 / 2 / Math.PI
-
-
-
-
-
+    x = input.magneticForce(Dimension.X)
+    z = input.magneticForce(Dimension.Z)
+    // forward facing: 0 if the north is facing the screen
+    rad = Math.atan2(x, 0 - z)
+    // let deg = rad * 360 / 2 / Math.PI
+    // 20 pixels / round
+    offset = Math.round(rad * 20 / 2 / Math.PI)
+    offset = (offset + 20) % 20
+    nwsen.showImage(offset)
 })
