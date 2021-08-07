3c048dfc050c088ab5b38d08e05d649d5b68e917
davidp443 <82887237+davidp443@users.noreply.github.com>
2021-08-07 09:01:59 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+basic.forever(function () {
+	let x = input.magneticForce(Dimension.X)
+    let z = input.magneticForce(Dimension.Z)
+    let rad = Math.atan2(x,-z)
+    //let deg = rad * 360 / 2 / Math.PI
+
+
+
+
+
+})
